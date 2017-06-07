f2a20122b2b035a7b3bf3dee244b7dc49dd41bcd
Dominic Brown and Mohamed Warsame <dominic@>
2017-06-07 09:24:19 -0700

    add skeleton files

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+// annotation, how it appears
+
+import { Component } from '@angular/core';
+
+
+// 'decorator'
+@Component({
+  selector: 'app-root',
+  template: `<h1>Tap Room template</h1>`
+})
+
+// class declaration, how it behaves
+export class AppComponent {
+
+}
